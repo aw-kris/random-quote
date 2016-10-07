052e984091ec403c74d6cbd584b5548d72317ef3
Kharisma Agung Wahono <aw.kharisma@gmail.com>
2016-10-07 09:59:08 +0700

    change back to previous http API call

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //function to get quote from API
 var getQuote = function() {
   $.ajaxSetup({cache:false});
-  $.getJSON('https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(json) {
+  $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(json) {
     $('#qText').html(json.quoteText);
     if(json.quoteAuthor==='') {
       $('#qAuthor').html('&mdash; Anonymous');
