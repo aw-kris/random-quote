5ec7bf58ec5658b70e99e373b8498d959c61a9eb
Kharisma Agung Wahono <aw.kharisma@gmail.com>
2016-10-07 08:20:08 +0700

    added index.js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+//function to get quote from API
+var getQuote = function() {
+  $.ajaxSetup({cache:false});
+  $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(json) {
+    $('#qText').html(json.quoteText);
+    if(json.quoteAuthor==='') {
+      $('#qAuthor').html('&mdash; Anonymous');
+    }
+    else {
+      $('#qAuthor').html('&mdash; ' + json.quoteAuthor);
+    }
+    $('#tweet').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&text='+json.quoteText+' - '+json.quoteAuthor);
+  });
+};
+
+$(document).ready(function() {
+  getQuote();
+  $('#new').on('click', getQuote);
+});
